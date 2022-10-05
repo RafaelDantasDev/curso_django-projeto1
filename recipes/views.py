1639b801178afa4df7e067d46d8ed8155380a779
RafaelDantasDev <dantas.rafael8928@gmail.com>
2022-10-04 21:46:26 -0300

    Removido contato e sebre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Rafael'
     })
-
-def contatos(request):
-    return HttpResponse('Cotato')
-
-def sobre(request):
-    return HttpResponse('Sobre')
